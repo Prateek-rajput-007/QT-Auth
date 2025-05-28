c990b121a33a34d7c003228d37c56d1faa0ccb1e
Prateek <109753518+Prateek-rajput-007@users.noreply.github.com>
2025-05-28 12:58:36 +0530

    Update LoginForm.jsx

--- Frontend/src/components/LoginForm.jsx
+++ Frontend/src/components/LoginForm.jsx
@@ -16,7 +16,7 @@ const LoginForm = () => {
     setLoading(true);
     setError('');
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', {
+      const response = await axios.post('https://qt-backend-delta.vercel.app/api/auth/login', {
         email,
         password,
       });
